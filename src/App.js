0273051e53f0ac64b37ef9eab0f502df7d580bb5
aliamk <alia.azarian@gmail.com>
2020-04-22 19:01:16 +0100

    Everything but the API added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Particles from 'react-particles-js';
 import './App.css';
 import Navigation from './Components/Navigation/Navigation.js'
 import Logo from './Components/Logo/Logo.js'
+import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm.js'
+import Rank from './Components/Rank/Rank.js'
 
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 200
+      },
+    }
+  }
+}
 
-function App() {
-  return (
-    <div className="App">
-      <Navigation />
-      <Logo />
-      <ImageLinkForm /> 
-      {/* <FaceRecognition />*/}
-    </div>
-  );
+class App extends Component {
+    render() {
+      return (
+        <div className="App">
+          <Particles className='particles'
+            params={particlesOptions}
+          />
+          <Navigation />
+          <Logo />
+          <Rank />
+          <ImageLinkForm />
+          {/* <FaceRecognition />*/}
+        </div>
+      );
+    }
 }
 
 export default App;
